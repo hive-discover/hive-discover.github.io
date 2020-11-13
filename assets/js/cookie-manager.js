8d02991b6b0ad9fbd88f0115eba0b5f58cbbc209
Christopher-06 <63494008+Christopher-06@users.noreply.github.com>
2020-11-13 20:11:11 +0100

    Adding delete cookie function

--- assets/js/cookie-manager.js
+++ assets/js/cookie-manager.js
@@ -27,3 +27,14 @@ function getCookie(cname) {
     else 
         return false;
   }
+
+  function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
+    }
+}
